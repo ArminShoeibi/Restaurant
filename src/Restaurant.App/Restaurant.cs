33cbdcd6256ab0a25b42a8372966ad418433da77
armin <a.shoeibi@alibaba.ir>
2021-11-05 19:45:29 +0330

    Add project files.

---
+++ src/Restaurant.App/Restaurant.cs
@@ -0,0 +1,18 @@
+﻿namespace Restaurant.App;
+
+public class Restaurant
+{
+    public int RestaurantId { get; set; }
+    public string EnglishName { get; set; }
+    public string PersianName { get; set; }
+    public string Address { get; set; }
+    public string PhoneNumber { get; set; }
+    public (int From, int To) OpeningHours { get; set; }
+
+    public bool IsOpen(int hour)
+    {
+        (int from, int to) = OpeningHours;
+        if (hour >= from && hour <= to) return true;
+        return false;
+    }
+}
---
+++ tests/Restaurant.App.Tests/RestaurantTests.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+
+namespace Restaurant.App.Tests;
+
+public class RestaurantTests
+{
+
+    [Fact]
+    public void IsOpen_WhenHourIsInInRange_ReturnsTrue()
+    {
+        Restaurant restaurant = new()
+        {
+            RestaurantId = 1,
+            PhoneNumber = "+100000",
+            Address = "USA",
+            EnglishName = "KFC",
+            PersianName = "کی اف سی",
+            OpeningHours = (From: 10, To: 22),
+        };
+
+        bool isOpen = restaurant.IsOpen(12);
+
+        Assert.True(isOpen);
+    }
+
+    [Fact]
+    public void IsOpen_WhenHourIsntInRange_ReturnsFalse()
+    {
+        Restaurant restaurant = new()
+        {
+            RestaurantId = 1,
+            PhoneNumber = "+100000",
+            Address = "USA",
+            EnglishName = "KFC",
+            PersianName = "کی اف سی",
+            OpeningHours = (From: 10, To: 22),
+        };
+
+        bool isOpen = restaurant.IsOpen(23);
+        Assert.False(isOpen);
+    }
+
+}
